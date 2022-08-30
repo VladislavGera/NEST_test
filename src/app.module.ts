012235048ecbd81f1d68605ea059ac42e364bded
login <vheranin@gmail.com>
2022-08-30 17:01:50 +0300

    add mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NewsController } from './news/news.controller';
 import { NewsService } from './service/products.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://vlad:plkiuplkiu@news.8nsiyan.mongodb.net/?retryWrites=true&w=majority',
+    ),
+  ],
   controllers: [AppController, NewsController],
   providers: [AppService, NewsService],
 })
